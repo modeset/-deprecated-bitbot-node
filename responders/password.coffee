aa0954a447e0676d43f2eb4003a4b1ff8076e2a4
Jay Zeschin <jay@zeschin.org>
2011-09-03 23:33:33 -0600

    Change password generator to use fishsticks service

--- responders/password.coffee
+++ responders/password.coffee
@@ -1,10 +1,13 @@
-child_process  = require('child_process')
+wwwdude = require("wwwdude")
+
+json_client = wwwdude.createClient({
+  contentParser: wwwdude.parsers.json
+})
 
 exports.helpMessage = "generate you a fresh new password when you say 'make me a password'"
 
 exports.receiveMessage = (message, room, client) ->
   if message.body and /^make me a password/.test( message.body )
-    child_process.exec('bundle exec ha-gen', (error, stdout, stderr) ->
-      unless error
-        room.speak 'Try this one: ' + stdout.replace(/^\s+|\s+$/g, "")
+   json_client.get('http://fishsticks.herokuapp.com/?wordlist=propernames').on('success', (data, response) ->
+      room.speak('Try this one: ' + data)
     )
--- responders/password.js
+++ responders/password.js
@@ -1,13 +1,14 @@
 (function() {
-  var child_process;
-  child_process = require('child_process');
+  var json_client, wwwdude;
+  wwwdude = require("wwwdude");
+  json_client = wwwdude.createClient({
+    contentParser: wwwdude.parsers.json
+  });
   exports.helpMessage = "generate you a fresh new password when you say 'make me a password'";
   exports.receiveMessage = function(message, room, client) {
     if (message.body && /^make me a password/.test(message.body)) {
-      return child_process.exec('bundle exec ha-gen', function(error, stdout, stderr) {
-        if (!error) {
-          return room.speak('Try this one: ' + stdout.replace(/^\s+|\s+$/g, ""));
-        }
+      return json_client.get('http://fishsticks.herokuapp.com/?wordlist=propernames').on('success', function(data, response) {
+        return room.speak('Try this one: ' + data);
       });
     }
   };
