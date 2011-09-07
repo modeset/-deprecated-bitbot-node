3228a23e23afbc37f5b600b658a938afa667640d
Matthew Kitt <mk.kitt@gmail.com>
2011-09-07 00:58:44 -0600

    reverting the js

--- responders/help.js
+++ responders/help.js
@@ -1,16 +1,19 @@
 (function() {
   exports.receiveMessage = function(message, room, client) {
-    var helps, responder, _i, _len, _ref;
-    helps = "yo dawg. need some help? here's what i can do:\n";
+    var responder, _i, _len, _ref, _results;
     if (message.body && message.body.match(/what up bot|help bit bot/)) {
+      room.speak("yo dawg. need some help? here's what i can do:");
       _ref = client.responders;
+      _results = [];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         responder = _ref[_i];
-        if (responder.helpMessage) {
-          helps += responder.helpMessage + '\n';
-        }
+        _results.push((function(responder) {
+          if (responder.helpMessage) {
+            return room.speak(responder.helpMessage);
+          }
+        })(responder));
       }
+      return _results;
     }
-    return room.speak(helps);
   };
 }).call(this);
