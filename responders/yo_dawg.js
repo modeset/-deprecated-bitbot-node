fb7f9b8735e9ff9b5ee03709da47d77d3c3dc22e
Matthew Kitt <mk.kitt@gmail.com>
2011-09-07 00:01:00 -0600

    Add random results to yo dawg

--- responders/yo_dawg.js
+++ responders/yo_dawg.js
@@ -1,7 +1,9 @@
 (function() {
   exports.receiveMessage = function(message, room, client) {
-    var hello_regex;
+    var hello_regex, random, results;
     hello_regex = /hello bit bot|yo dawg|sup bit bot|morning bit bot/;
+    results = ['yo dawg', '0110100001101001', 'well hello there'];
+    random = results[Math.floor(Math.random() * results.length)];
     if (message.userId !== client.bitBotId && message.body && message.body.match(hello_regex)) {
       return room.speak("yo dawg");
     }
