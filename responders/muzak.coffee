90ae8dbbb14fee9abb422ac0e9b7539cf6f0e82e
Jay Zeschin <jay@zeschin.org>
2011-09-06 23:47:52 -0600

    Refactor plus fix style URL

--- responders/muzak.coffee
+++ responders/muzak.coffee
@@ -7,6 +7,10 @@ json_client = wwwdude.createClient({
 exports.helpMessage = "recommend you some music when you say 'find me some <mood> tunes' or 'find me some <style> music'"
 
 exports.receiveMessage = (message, room) ->
+  respondToTunes(message,room)
+  respondToMusic(message,room)
+
+exports.respondToTunes = (message, room) ->
   msg_regex = /^find me some (.+) tunes/
   if message.body and msg_regex.test( message.body )
     regex_results = msg_regex.exec(message.body)
@@ -19,11 +23,13 @@ exports.receiveMessage = (message, room) ->
       else
         room.speak "sorry, couldn't find any #{mood} tunes for you"
     )
+
+exports.respondToMusic = (message, room) ->
   msg_regex = /^find me some (.+) music/
   if message.body and msg_regex.test( message.body )
     regex_results = msg_regex.exec(message.body)
     style = regex_results[1]
-    request_url = "http://developer.echonest.com/api/v4/song/search?api_key=#{process.env.ECHONEST_API_KEY}&format=json&results=1&style=#{mood}&sort=artist_hotttnesss-desc"
+    request_url = "http://developer.echonest.com/api/v4/song/search?api_key=#{process.env.ECHONEST_API_KEY}&format=json&results=1&style=#{style}&sort=artist_hotttnesss-desc"
     json_client.get(request_url).on('success', (data, response) ->
       if(data.response.songs.length > 0)
         song = data.response.songs[0]
--- responders/muzak.js
+++ responders/muzak.js
@@ -6,13 +6,17 @@
   });
   exports.helpMessage = "recommend you some music when you say 'find me some <mood> tunes' or 'find me some <style> music'";
   exports.receiveMessage = function(message, room) {
-    var mood, msg_regex, regex_results, request_url, style;
+    respondToTunes(message, room);
+    return respondToMusic(message, room);
+  };
+  exports.respondToTunes = function(message, room) {
+    var mood, msg_regex, regex_results, request_url;
     msg_regex = /^find me some (.+) tunes/;
     if (message.body && msg_regex.test(message.body)) {
       regex_results = msg_regex.exec(message.body);
       mood = regex_results[1];
       request_url = "http://developer.echonest.com/api/v4/song/search?api_key=" + process.env.ECHONEST_API_KEY + "&format=json&results=1&mood=" + mood + "&sort=artist_hotttnesss-desc";
-      json_client.get(request_url).on('success', function(data, response) {
+      return json_client.get(request_url).on('success', function(data, response) {
         var song;
         if (data.response.songs.length > 0) {
           song = data.response.songs[0];
@@ -22,11 +26,14 @@
         }
       });
     }
+  };
+  exports.respondToMusic = function(message, room) {
+    var msg_regex, regex_results, request_url, style;
     msg_regex = /^find me some (.+) music/;
     if (message.body && msg_regex.test(message.body)) {
       regex_results = msg_regex.exec(message.body);
       style = regex_results[1];
-      request_url = "http://developer.echonest.com/api/v4/song/search?api_key=" + process.env.ECHONEST_API_KEY + "&format=json&results=1&style=" + mood + "&sort=artist_hotttnesss-desc";
+      request_url = "http://developer.echonest.com/api/v4/song/search?api_key=" + process.env.ECHONEST_API_KEY + "&format=json&results=1&style=" + style + "&sort=artist_hotttnesss-desc";
       return json_client.get(request_url).on('success', function(data, response) {
         var song;
         if (data.response.songs.length > 0) {
