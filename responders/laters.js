a72beea69c458d689fed8de3699b5dea91e55627
Jay Zeschin <jay@zeschin.org>
2011-09-07 23:40:23 -0600

    Nuke a stub JS file

--- responders/laters.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  exports.receiveMessage = function(message, room, client) {
-    var hello_regex;
-    hello_regex = /laters bit bot|night bit bot/;
-    if (message.userId !== client.bitBotId && message.body && message.body.match(hello_regex)) {
-      return room.speak("g'night slacker");
-    }
-  };
-}).call(this);
