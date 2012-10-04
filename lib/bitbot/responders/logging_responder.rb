82c544d7c7ef2117c8d31f5d5810f0dcf7ceae48
Jay Zeschin <jay.zeschin@modeset.com>
2012-10-03 23:17:53 -0600

    Improve logging

--- lib/bitbot/responders/logging_responder.rb
+++ lib/bitbot/responders/logging_responder.rb
@@ -2,7 +2,7 @@ module Bitbot
   module Responders
     class LoggingResponder < BaseResponder
       def receive_message(message)
-        puts "#{@room_binding.room.name} heard #{message.inspect}"
+        puts "#{@room_binding.room.name} heard #{message.body} from #{message.user.name}"
       end
     end
   end
